5792fd785410c6c9e6e1e33a73ec31c634c44e04
Alberto Gómez Morales <100029355+ElPediatra@users.noreply.github.com>
2023-03-21 23:28:17 +0100

    Add files via upload

---
+++ Alberto_Entornos/Tema3/Práctica 1/diagrama3.java
@@ -0,0 +1,9 @@
+(I) public boolean divisible(int multiplo, int divisor) {
+(1)      boolean resultado;
+
+(2)         if (multiplo % divisor == 0){
+(3)             resultado = true;
+            } else {
+(4)             resultado = false;
+            }
+(F)  }
