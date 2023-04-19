37533383d14d378ec7e599090f036f2d37875786
Alberto Gómez Morales <100029355+ElPediatra@users.noreply.github.com>
2023-04-19 09:40:33 +0200

    Add files via upload

--- Alberto_BBDD/Tema5/Consultas 2/consultas2.sql
+++ Alberto_BBDD/Tema5/Consultas 2/consultas2.sql
@@ -2,19 +2,19 @@
 
 SELECT NombreCliente
 FROM Clientes
-WHERE LimiteCredito = (SELECT MAX(LimiteCredito) FROM Clientes)
+WHERE LimiteCredito = (SELECT MAX(LimiteCredito) FROM Clientes);
 
 /* 2. Obtener el nombre, apellido1 y puesto de los empleados que no representen a ningún cliente. */
 
 SELECT NombreEmpleado, Apellido1, Puesto
 FROM Empleados
-WHERE IdEmpleado NOT IN (SELECT DISTINCT IdEmpleado FROM Clientes)
+WHERE IdEmpleado NOT IN (SELECT DISTINCT IdEmpleado FROM Clientes);
 
 /* 3. Obtener el nombre del producto más caro. */
 
 SELECT NombreProducto
 FROM Productos
-WHERE PrecioUnitario = (SELECT MAX(PrecioUnitario) FROM Productos)
+WHERE PrecioUnitario = (SELECT MAX(PrecioUnitario) FROM Productos);
 
 /* 4. Obtener el nombre del producto del que más unidades se hayan vendido en un mismo pedido. */
 
@@ -26,7 +26,7 @@ WHERE IdProducto = (
   GROUP BY IdProducto
   ORDER BY SUM(Cantidad) DESC
   LIMIT 1
-)
+);
 
 /* 5. Obtener los clientes cuya línea de crédito sea mayor que los pagos que haya realizado. */
 
@@ -36,7 +36,7 @@ WHERE LimiteCredito > (
   SELECT IFNULL(SUM(Cantidad), 0)
   FROM Pagos
   WHERE Pagos.IdCliente = Clientes.IdCliente
-)
+);
 
 /* 6. Sacar el producto que más unidades tiene en stock y el que menos unidades tiene en stock. */
 
@@ -48,13 +48,13 @@ WHERE UnidadesEnStock = (
 ) OR UnidadesEnStock = (
   SELECT MIN(UnidadesEnStock)
   FROM Productos
-)
+);
 
 /* 7. Sacar la media de unidades en stock de los productos agrupados por gama. */
 
 SELECT Gama, AVG(UnidadesEnStock)
 FROM Productos
-GROUP BY Gama
+GROUP BY Gama;
 
 /* 8. Sacar los clientes que residan en ciudades donde no hay oficinas ordenado por la ciudad donde residen. */
 
@@ -63,13 +63,13 @@ FROM Clientes
 WHERE Ciudad NOT IN (
   SELECT DISTINCT Ciudad
   FROM Oficinas
-) ORDER BY Ciudad
+) ORDER BY Ciudad;
 
 /* 9. Sacar un listado con el nombre de cada cliente y el nombre y apellido de su representante de ventas. */
 
 SELECT c.NombreCliente, e.NombreEmpleado, e.Apellido1
 FROM Clientes c
-JOIN Empleados e ON c.IdEmpleado = e.IdEmpleado
+JOIN Empleados e ON c.IdEmpleado = e.IdEmpleado;
 
 /* 10. Mostrar el nombre de los clientes que no hayan realizado pagos junto con el nombre de su representante de ventas. */
 
@@ -79,7 +79,7 @@ JOIN Empleados e ON c.IdEmpleado = e.IdEmpleado
 WHERE c.IdCliente NOT IN (
   SELECT DISTINCT IdCliente
   FROM Pagos
-)
+);
 
 /* 11. Listar las ventas totales de los productos que hayan facturado más de 3000 euros. Se mostrará el nombre, unidades vendidas, total facturado y total facturado con impuestos (21% IVA). */
 
@@ -87,7 +87,7 @@ SELECT p.NombreProducto, SUM(d.Cantidad) AS UnidadesVendidas,
   ROUND(SUM(d.PrecioUnitario*d.Cantidad), 2) AS TotalFacturado,
   ROUND(SUM(d.PrecioUnitario*d.Cantidad*1.21), 2) AS TotalFacturadoIVA
 FROM Productos p
-JOIN DetallesPedidos d ON p.IdProducto = d.Id
+JOIN DetallesPedidos d ON p.IdProducto = d.Id;
 
 /* 12. Listar la dirección de las oficinas que tengan clientes en Fuenlabrada. */
 
@@ -145,3 +145,117 @@ INNER JOIN pedido ON cliente.id_cliente = pedido.id_cliente
 GROUP BY cliente.nombre
 ORDER BY max_cuantia DESC
 LIMIT 1;
+
+/* 18. Sacar el importe medio de los pedidos. */
+
+SELECT AVG(Cantidad * PrecioUnidad) AS ImporteMedio
+FROM DetallePedidos;
+
+/* 19. Sacar un listado de clientes indicando el nombre del cliente y cuántos pedidos ha realizado. */
+
+SELECT NombreCliente, COUNT(*) AS NumeroPedidos
+FROM Clientes c
+INNER JOIN Pedidos p ON c.CodigoCliente = p.CodigoCliente
+GROUP BY NombreCliente;
+
+/* 20. Sacar un listado con los nombres de los clientes y el total pagado por cada uno de ellos. */
+
+SELECT NombreCliente, SUM(Cantidad) AS TotalPagado
+FROM Clientes c
+INNER JOIN Pagos p ON c.CodigoCliente = p.CodigoCliente
+GROUP BY NombreCliente;
+
+/* 21. Sacar el nombre de los clientes que hayan hecho pedidos en 2008. */
+
+SELECT DISTINCT NombreCliente
+FROM Clientes c
+INNER JOIN Pedidos p ON c.CodigoCliente = p.CodigoCliente
+WHERE YEAR(FechaPedido) = 2008;
+
+/* 22. Listar el nombre del cliente y el nombre y apellido de sus representantes de aquellos que no hayan realizado pagos. */
+
+SELECT c.NombreCliente, e.Nombre, e.Apellido1
+FROM Clientes c
+LEFT JOIN Pagos p ON c.CodigoCliente = p.CodigoCliente
+INNER JOIN Empleados e ON c.CodigoEmpleadoRepVentas = e.CodigoEmpleado
+WHERE p.IDTransaccion IS NULL;
+
+/* 23. Sacar un listado de clientes donde aparezca el nombre de su comercial y la ciudad donde está su oficina. */
+
+SELECT c.NombreCliente, e.Nombre, e.Apellido1, o.Ciudad
+FROM Clientes c
+INNER JOIN Empleados e ON c.CodigoEmpleadoRepVentas = e.CodigoEmpleado
+INNER JOIN Oficinas o ON e.CodigoOficina = o.CodigoOficina;
+
+/* 24. Sacar el nombre, apellidos, oficina y cargo de aquellos que no sean representantes de ventas. */
+
+SELECT Nombre, Apellido1, Apellido2, Oficinas.Ciudad, Oficinas.Pais, Puesto
+FROM Empleados
+LEFT JOIN Oficinas ON Empleados.CodigoOficina = Oficinas.CodigoOficina
+WHERE CodigoEmpleado NOT IN (SELECT CodigoEmpleadoRepVentas FROM Clientes WHERE CodigoEmpleadoRepVentas IS NOT NULL);
+
+/* 25. Sacar cuantos empleados tiene cada oficina, mostrando el nombre de la ciudad donde está la oficina. */
+
+SELECT Oficinas.Ciudad, COUNT(*) AS NumeroEmpleados
+FROM Empleados
+INNER JOIN Oficinas ON Empleados.CodigoOficina = Oficinas.CodigoOficina
+GROUP BY Oficinas.Ciudad;
+
+/* 26. Sacar los clientes que residan en la misma ciudad donde hay una oficina, indicando dónde está la oficina. */
+
+SELECT c.nombre AS nombre_cliente, c.ciudad AS ciudad_cliente, o.ciudad AS ciudad_oficina
+FROM clientes c 
+JOIN empleados e ON c.comercial_asignado = e.codigo_empleado
+JOIN oficinas o ON e.codigo_oficina = o.codigo_oficina
+WHERE c.ciudad = o.ciudad;
+
+/* 27. Sacar el número de clientes que tiene asignado cada representante de ventas. */
+
+SELECT r.nombre AS representante, COUNT(*) AS num_clientes
+FROM clientes c
+INNER JOIN representantes r ON c.representante_id = r.id
+GROUP BY r.id;
+
+/* 28. Sacar cuál fue el cliente que hizo el pago con mayor cuantía y el que hizo el pago con menor cuantía. */
+
+SELECT c.nombre AS cliente_max, MAX(p.importe) AS importe_max, 
+       c2.nombre AS cliente_min, MIN(p.importe) AS importe_min
+FROM clientes c 
+INNER JOIN pedidos p ON c.id = p.cliente_id
+INNER JOIN clientes c2 ON c2.id = p.cliente_id
+GROUP BY c.id, c2.id
+ORDER BY importe_max DESC, importe_min ASC
+LIMIT 1;
+
+/* 29. Sacar un listado con el precio total de cada pedido. */
+
+SELECT p.id AS pedido, SUM(d.cantidad * d.precio_unitario) AS total
+FROM pedidos p 
+INNER JOIN detalles_pedido d ON p.id = d.pedido_id
+GROUP BY p.id;
+
+/* 30. Sacar los clientes que hayan hecho pedidos en el 2008 por una cuantía superior a 2000 euros. */
+
+SELECT c.nombre, SUM(d.cantidad * d.precio_unitario) AS total
+FROM clientes c 
+INNER JOIN pedidos p ON c.id = p.cliente_id
+INNER JOIN detalles_pedido d ON p.id = d.pedido_id
+WHERE YEAR(p.fecha_pedido) = 2008
+GROUP BY c.id
+HAVING total > 2000;
+
+/* 31. Sacar cuantos pedidos tiene cada cliente en cada estado. */
+
+SELECT customers.customerName, orders.status, COUNT(*) as num_orders
+FROM customers
+INNER JOIN orders ON customers.customerNumber = orders.customerNumber
+GROUP BY customers.customerNumber, orders.status
+ORDER BY customers.customerName, orders.status;
+
+/* 32. Sacar los clientes que han pedido más de 200 unidades de cualquier producto. */
+
+SELECT DISTINCT customers.customerName
+FROM customers
+INNER JOIN orders ON customers.customerNumber = orders.customerNumber
+INNER JOIN orderdetails ON orders.orderNumber = orderdetails.orderNumber
+WHERE orderdetails.quantity > 200;
--- Alberto_BBDD/Tema5/Consultas 2/consultas3.sql
+++ Alberto_BBDD/Tema5/Consultas 2/consultas3.sql
@@ -116,41 +116,134 @@ FROM Partidos
 JOIN Equipos ON Partidos.Equipo_local = Equipos.Nombre
 WHERE Equipos.Conferencia = 'Oeste' GROUP BY Equipo_local
 UNION SELECT Equipo_visitante AS "Equipo", COUNT(CASE WHEN Puntos_visitante > Puntos_local THEN 1 END) + COUNT(CASE WHEN Puntos_visitante = Puntos_local THEN 0.5 END) AS "wins"
-FROM Partidos
+FROM Partidos;
 
 /* 21. Equipo y ciudad de los jugadores españoles de la NBA. */
 
+SELECT Jugadores.Nombre_equipo, Equipos.Ciudad
+FROM Jugadores
+JOIN Equipos ON Jugadores.Nombre_equipo = Equipos.Nombre
+WHERE Jugadores.Procedencia LIKE '%Spain%';
 
 /* 22. Puntos por partido de 'Pau Gasol' en toda su carrera. */
 
+SELECT AVG(Puntos_por_partido) AS "Puntos por partido de Pau Gasol en su carrera"
+FROM Estadisticas
+JOIN Jugadores ON Estadisticas.Jugador = Jugadores.Codigo
+WHERE Jugadores.Nombre = 'Pau Gasol';
+
 /* 23. Puntos por partido de los jugadores de los 'cavaliers'. */
+
+SELECT AVG(Puntos_por_partido) AS "Puntos por partido de los jugadores de los Cavaliers"
+FROM Estadisticas
+JOIN Jugadores ON Estadisticas.Jugador = Jugadores.Codigo
+JOIN Equipos ON Jugadores.Nombre_equipo = Equipos.Nombre
+WHERE Equipos.Nombre LIKE '%Cavaliers%';
 
 /* 24. Número de jugadores que tiene cada equipo de la conferencia oeste ('west'). */
 
+SELECT Equipos.Nombre, COUNT(*) AS "Número de jugadores"
+FROM Jugadores
+JOIN Equipos ON Jugadores.Nombre_equipo = Equipos.Nombre
+WHERE Equipos.Conferencia = 'West'
+GROUP BY Equipos.Nombre;
+
 /* 25.  Asistencias por partido de 'José Calderón' en la temporada '07/08'. */
+
+SELECT Asistencias_por_partido AS "Asistencias por partido de José Calderón en la temporada 07/08"
+FROM Estadisticas
+JOIN Jugadores ON Estadisticas.Jugador = Jugadores.Codigo
+WHERE Jugadores.Nombre = 'José Calderón' AND Estadisticas.Temporada = '07/08';
 
 /* 26. Puntos por partido de 'LeBron James' en las temporadas del '03/04' al '05/06'. */
 
+SELECT AVG(Puntos_por_partido) AS "Puntos por partido de LeBron James del 03/04 al 05/06"
+FROM Estadisticas
+JOIN Jugadores ON Estadisticas.Jugador = Jugadores.Codigo
+WHERE Jugadores.Nombre = 'LeBron James' AND Estadisticas.Temporada BETWEEN '03/04' AND '05/06';
+
 /* 27. Número de jugadores que tiene cada equipo en la conferencia este ('east'). */
+
+SELECT Equipos.Nombre, COUNT(*) AS "Número de jugadores"
+FROM Jugadores
+JOIN Equipos ON Jugadores.Nombre_equipo = Equipos.Nombre
+WHERE Equipos.Conferencia = 'East'
+GROUP BY Equipos.Nombre;
 
 /* 28. Tapones por partido de los jugadores de los ' Trail Blazers'. */
 
+SELECT AVG(Tapones_por_partido) AS "Tapones por partido de los jugadores de los Trail Blazers"
+FROM Estadisticas
+JOIN Jugadores ON Estadisticas.Jugador = Jugadores.Codigo
+JOIN Equipos ON Jugadores.Nombre_equipo = Equipos.Nombre
+WHERE Equipos.Nombre LIKE '%Trail Blazers%';
+
 /* 29. Media de rebotes de los jugadores de la conferencia este ('east'). */
+
+SELECT AVG(rebotesPorPartido)
+FROM Jugador
+WHERE conferencia = 'East';
 
 /* 30. Rebotes por partido de los jugadores de los equipos de Los Angeles. */
 
+SELECT AVG(rebotes_por_partido) AS promedio_rebotes, equipo, ciudad
+FROM jugadores
+WHERE equipo LIKE '%Lakers%' OR equipo LIKE '%Clippers%'
+GROUP BY equipo, ciudad;
+
 /* 31. Número de jugadores que tiene cada equipo en la división NorthWest. */
+
+SELECT COUNT(*) AS numero_jugadores, equipo
+FROM jugadores
+WHERE division = 'Northwest'
+GROUP BY equipo;
 
 /* 32. Jugadores que no metieron ningún punto en alguna temporada. */
 
+SELECT nombre, MIN(puntos_por_partido)
+FROM jugadores
+GROUP BY nombre
+HAVING MIN(puntos_por_partido) = 0;
+
 /* 33. Número total de jugadores de cada división. */
+
+SELECT COUNT(*) AS numero_jugadores, division
+FROM jugadores
+GROUP BY division;
 
 /* 34. Temporada con más puntos por partido de 'Kobe Bryant'. */
 
+SELECT MAX(puntos_por_partido) AS max_puntos, temporada
+FROM jugadores
+WHERE nombre = 'Kobe Bryant'
+GROUP BY temporada
+ORDER BY max_puntos DESC
+LIMIT 1;
+
 /* 35. Número de bases 'G' que tiene cada equipo de la conferencia este 'East'. */
+
+SELECT COUNT(*) AS numero_bases, equipo
+FROM jugadores
+WHERE posicion LIKE '%G' AND conferencia = 'East'
+GROUP BY equipo;
 
 /* 36. Máximo reboteador de los 'Suns'. */
 
+SELECT MAX(rebotes_por_partido) AS max_rebotes, nombre
+FROM jugadores
+WHERE equipo = 'Suns';
+
 /* 37. Máximo anotador de toda la base de datos en una temporada. */
 
+SELECT MAX(puntos_por_partido) AS max_puntos, nombre, equipo, temporada
+FROM jugadores
+GROUP BY nombre, equipo, temporada
+ORDER BY max_puntos DESC
+LIMIT 1;
+
 /* 38.  ¿Cuantas letras tiene el equipo con nombre más largo de la NBA (Ciudad y Nombre)?. */
+
+SELECT LENGTH(equipo) AS numero_letras, equipo
+FROM jugadores
+ORDER BY numero_letras DESC
+LIMIT 1;
