a812a6e4c687ee555f7e510bfff7bcc0fc642af9
Alberto Gómez Morales <100029355+ElPediatra@users.noreply.github.com>
2023-05-10 14:01:40 +0200

    Add files via upload

---
+++ Alberto_Programacion/Tercer_Trimestre/Tema_8-Herencias/U8_R2_ArrayList/U8_R2_ArrayList_GomezMorales_Alberto/src/Animales/Sexo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ * Clase enumerada solo para Sexo
+ * @author alber
+ */
+
+/* Paquetes */
+package Animales;
+
+public enum Sexo {
+    Macho,Hembra,Hermafrodita
+}
